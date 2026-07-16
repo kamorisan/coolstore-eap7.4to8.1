0ea7a652ce1d9e24098fc95ef2c3ec0ad837878e
kamori <kamori@redhat.com>
2026-07-16 13:36:13 +0900

    Revert "Comment out JMSDestinationDefinition annotation (use standalone-full.xml config instead)"
    This reverts commit 906dbd674282e46065eb2b595d95d3828ba3d478.

--- src/main/java/com/redhat/coolstore/rest/CartEndpoint.java
+++ src/main/java/com/redhat/coolstore/rest/CartEndpoint.java
@@ -8,8 +8,8 @@ import java.util.Map;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
-// import javax.jms.JMSDestinationDefinition;
-// import javax.jms.JMSDestinationDefinitions;
+import javax.jms.JMSDestinationDefinition;
+import javax.jms.JMSDestinationDefinitions;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -25,16 +25,15 @@ import com.redhat.coolstore.service.ShoppingCartService;
 
 @SessionScoped
 @Path("/cart")
-// JMS destination is defined in standalone-full.xml
-// @JMSDestinationDefinitions(
-// 	value = {
-// 		@JMSDestinationDefinition(
-// 			name = "java:/jms/queue/orders",
-// 			interfaceName = "javax.jms.Queue",
-// 			destinationName = "ordersQueue"
-// 		)
-// 	}
-// )
+@JMSDestinationDefinitions(
+	value = {
+		@JMSDestinationDefinition(
+			name = "java:/jms/queue/orders",
+			interfaceName = "javax.jms.Queue",
+			destinationName = "ordersQueue"
+		)
+	}
+)
 public class CartEndpoint implements Serializable {
 
 	private static final long serialVersionUID = -7227732980791688773L;
